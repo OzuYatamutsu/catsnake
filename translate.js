8ef6a4af8a58ab8d2fe526119127c532597edc21
Sean Collins <seanc@spiceworks.com>
2016-03-08 12:33:42 -0600

    Add timescale

--- translate.js
+++ translate.js
@@ -5,3 +5,15 @@ var translate = {
   "wait": "waitForExist('{0}', {1})",
   "return": "return '{0}'"
 }
+
+var timescales = {
+  "ms": 1,
+  "millisecond": 1,
+  "milliseconds": 1,
+  "s": 1000,
+  "second": 1000,
+  "seconds": 1000,
+  "m": 60000,
+  "minute": 60000,
+  "minutes": 60000
+}
